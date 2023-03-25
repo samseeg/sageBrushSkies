6a38530c2559cc815c2059e1c3f7b7ad10dfc649
Sam <samseegmiller@gmail.com>
2023-03-25 15:34:12 -0600

    first commit

---
+++ sagebrushskies/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'vcepn1e0',
+    dataset: 'production'
+  }
+})
---
+++ sagebrushskies/schemas/index.ts
@@ -0,0 +1,4 @@
+import product from './photo';
+import socialImages from './socialImages';
+
+export const schemaTypes = [product, socialImages]
