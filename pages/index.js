a17a51e8d59128a0a9fb89034524d79abdf9d8f6
Sam <samseegmiller@gmail.com>
2023-04-02 22:42:13 -0600

    setup sanity and can now use data from sanity on front end

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,19 @@
 import React from 'react'
 
-const Home = () => {
+import {client} from '../lib/client';
+import {Header} from '../components/Header';
+
+const Home = ({products}) => {
   return (
     <>
-      Header
+      <Header />
 
       <div className='heroWrapper'>
 
       </div>
 
       <div className='galleryWrapper'>
-
+          {products?.map((product) => product.name)}
       </div>
 
       <div className='followMeWrapper'>
@@ -20,6 +23,15 @@ const Home = () => {
       Footer
     </>
   )
+}
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  return {
+    props: {products}
+  }
 }
 
 export default Home
